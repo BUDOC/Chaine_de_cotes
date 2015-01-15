941909158b21de468b53f9f5d15c141a13ec32a3
BUDOC <budoc@wanadoo.fr>
2015-01-15 21:47:23 +0100

    maj

---
+++ WindowsFormsApplication1/Communs.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+  
+    public class cond //declaration
+    {
+        public string nom;   // déclaration des variables publiques
+        public int condOrigine;
+        public int condExtremite;
+        public bool traite;
+        public float ITcond;
+        public float cmoycond;
+
+        public cond(string n, int o, int e, float cmoy, float IT, bool vu) // constructeur  des conditions  
+        {
+            nom = n;
+            condOrigine = o;
+            condExtremite = e;
+            traite = vu;
+            ITcond = IT;
+            cmoycond = cmoy;
+        }
+
+        //============= getteurs et setteurs de conditions =============
+        public string condName
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public int conditionOrigine
+        {
+            get { return condOrigine; }
+            set { condOrigine = value; }
+        }
+        public int conditionExtremite
+        {
+            get { return condExtremite; }
+            set { condExtremite = value; }
+        }
+        public float conditionCmoy
+        {
+            get { return cmoycond; }
+            set { cmoycond = value; }
+        }
+        public float conditionIT
+        {
+            get { return ITcond; }
+            set { ITcond = value; }
+        }
+        public bool conditionTraitee
+        {
+            get { return traite; }
+            set { traite = value; }
+        }
+    }
+
+
+    public class cf // declaration cf
+    {
+        public string cfname;
+        public int cfOrigine;
+        public int cfExtremite;
+        public bool cfTraite;
+        public float cfIT;
+        public float cfCmoy;
+        public float cfDlOrigine;
+        public float cfDlExtremite;
+
+        public cf(string nom, int cfO, int cfE, float cmoy, float IT, float DlO, float DlE, bool vu) // constructeur Cf
+        {
+            cfname = nom;
+            cfOrigine = cfO;
+            cfExtremite = cfE;
+            cfTraite = vu;
+            cfIT = IT;
+            cfCmoy = cmoy;
+            cfDlOrigine = DlO;
+            cfDlExtremite = DlE;
+        }
+
+
+
+    }
+
+   
+        
+        
+    }
+
+
+
+
+
+
+
+
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public cond C1;
+        public cf cf1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            C1 = new cond("C1", 2, 0, 50.2f, 1.3f, true);    //cond (string n, int o, int e,double cmoy,double IT, bool vu) 
+            cf1 = new cf("cf1",5,4,20.1f,0.56f,0.2f,0.3f,false);
+            // cf (string nom, int cfO, int cfE, float cmoy, float IT, float DlO, float DlE, bool vu) // constructeur Cf
+            infoCond(C1);
+            iniTab();
+ 
+        }
+        public void infoCond(cond C)
+        {
+            MessageBox.Show("nom condition = " + C1.condName + "\n" + "origine  " + C1.condOrigine.ToString() + " \nExtrémité  " + C1.conditionExtremite.ToString() + "\nCote moyenne = " + C1.conditionCmoy.ToString() + "\n IT = " + C1.conditionIT.ToString() + "\n traite =" + C1.conditionTraitee.ToString());
+            MessageBox.Show("nom condition = " + cf1.cfname + "\n" + "origine  " + cf1.cfOrigine + " \nExtrémité  " + cf1.cfExtremite + " \ncote moyenne   " +cf1.cfCmoy + " \nIT   " + cf1.cfIT + " \nDl origine  " +cf1.cfDlOrigine + " \n Dl Extrémité  " +cf1.cfExtremite+ "\n traite = " +cf1.cfTraite );
+            //int a = -999;   // pour mise au point message d'erreur
+            // testInt("-12.", a);
+        }
+        public void testInt(string s, int i)
+        {
+            bool result = int.TryParse(s, out i);
+            if (result) { result = true; } else { MessageBox.Show(s + " n'est pas un entier"); }
+
+        }
+      
+        public cf[] tabcf = new cf[10];
+        public void iniTab()
+        {
+            MessageBox.Show(tabcf.Count().ToString());
+            for (int i = 0; i <= tabcf.Count()-1; i++)
+            {
+                tabcf[i] = null;
+            }
+        }
+    }
+}
+
+
