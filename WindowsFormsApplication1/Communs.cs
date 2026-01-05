0772baefd3620dc0a169f8d99aaaaa7e032caa78
Old Dino <viotg@rocketmail.com>
2026-01-05 21:01:30 +0100

    Update WindowsFormsApplication1/Communs.cs — replace contents (was blob 4720c091d9e90f9c3d3f7e58f55fe7c4741bd81e)

--- WindowsFormsApplication1/Communs.cs
+++ WindowsFormsApplication1/Communs.cs
@@ -1,129 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace WindowsFormsApplication1
 {
-  
-    public class cond //declaration
+    public class Cond
     {
-       string nom;   // déclaration des variables publiques
-        public int condOrigine;
-         int condExtremite;
-         float ITcond;
-         float cmoycond;
+        // Propriétés publiques pour une condition
+        public string Name { get; set; }
+        public int Origine { get; set; }
+        public int Extremite { get; set; }
+        public float Cmoy { get; set; }
+        public float IT { get; set; }
 
-        public cond(string n, int o, int e, float cmoy, float IT) // constructeur  des conditions  
+        public Cond() { }
+
+        public Cond(string name, int origine, int extremite, float cmoy, float it)
         {
-            nom = n;
-            condOrigine = o;
-            condExtremite = e;          
-            ITcond = IT;
-            cmoycond = cmoy;
+            Name = name;
+            Origine = origine;
+            Extremite = extremite;
+            Cmoy = cmoy;
+            IT = it;
         }
-        public cond()
-        {
-            //constructeur vide
-        }
-        //============= getteurs et setteurs de conditions =============
-        public string condName
-        {
-            get { return nom; }
-            set { nom = value; }
-        }
-        public int conditionOrigine
-        {
-            get { return condOrigine; }
-            set { condOrigine = value; }
-        }
-        public int conditionExtremite
-        {
-            get { return condExtremite; }
-            set { condExtremite = value; }
-        }
-        public float conditionCmoy
-        {
-            get { return cmoycond; }
-            set { cmoycond = value; }
-        }
-        public float conditionIT
-        {
-            get { return ITcond; }
-            set { ITcond = value; }
-        }
-       
     }
 
-
-    public class cf // declaration cf
+    public class Cf
     {
-        public string cfname;
-        public int cfOrigine;
-        public int cfExtremite;
-        public bool cfTraite;
-        public float cfIT;
-        public float cfCmoy;
-        public float cfDlOrigine;
-        public float cfDlExtremite;
+        // Propriétés publiques pour un couple de fonction (cf)
+        public string Name { get; set; }
+        public int Origine { get; set; }
+        public int Extremite { get; set; }
+        public bool Traite { get; set; }
+        public float IT { get; set; }
+        public float CoteMoyenne { get; set; }
+        public float DlOrigine { get; set; }
+        public float DlExtremite { get; set; }
 
-        public cf() // constructeur vide
+        public Cf() { }
+
+        public Cf(string name, int origine, int extremite, float cmoy, float dlOrigine, float dlExtremite)
         {
-        }
-        
-        public cf(string nomcf, int cfO, int cfE, float cmoy, float DlO, float DlE) // constructeur Cf
-        {
-            cfname = nomcf;
-            cfOrigine = cfO;
-            cfExtremite = cfE;                  
-            cfCmoy = cmoy;
-            cfDlOrigine = DlO;
-            cfDlExtremite = DlE;              
-        }
-        //============= getteurs et setteurs de conditions =============
-        public string Name
-        {
-            get { return cfname; }
-            set { cfname= value; }
-        }
-        public int Origine
-        {
-            get { return cfOrigine; }
-            set { cfOrigine = value; }
-        }
-        public int Extremite
-        {
-            get { return cfExtremite; }
-            set { cfExtremite = value; }
-        }
-        public float CoteMoyenne
-        {
-            get { return cfCmoy; }
-            set { cfCmoy = value; }
-        }
-        public float DipersionOrigine
-        {
-            get { return cfDlOrigine; }
-            set { cfDlOrigine = value; }
-        }
-        public float DipersionExtremite
-        {
-            get { return cfDlExtremite; }
-            set { cfDlExtremite = value; }
+            Name = name;
+            Origine = origine;
+            Extremite = extremite;
+            CoteMoyenne = cmoy;
+            DlOrigine = dlOrigine;
+            DlExtremite = dlExtremite;
         }
     }
-
-   
-        
-        
-    }
-
-
-
-
-
-
-
-
-
+}
